b5dd567e5908bd2432d0c9f602d5b6984dcc2614
Ernest Wambua <ernestwambua2@gmail.com>
2022-07-14 21:02:07 +0300

    Finished on the hardware code

--- gps-tracker/gps-tracker.ino
+++ gps-tracker/gps-tracker.ino
@@ -1,7 +1,17 @@
+/*
+* Author: Ernest Wambua
+* Date: 14/07/2022
+*/
+
+#include <ArduinoJson.h>
+#include <TinyGPSPlus.h>
+#include <SoftwareSerial.h>
 #include <ArduinoMqttClient.h> // MQTT library
 #include <WiFiNINA.h> // Wi-Fi library
 #include "wifi_secrets.h"
 
+const int RxPin = 0, TxPin = 1;
+const uint32_t GPSBaud = 4800;
 char *wifi_ssid = WIFI_SSID;
 char *wifi_password = WIFI_PASSWORD;
 const char *mqtt_broker = "test.mosquitto.org"; // mqtt broker
@@ -11,11 +21,15 @@ const int interval =  8000; // interval for sending messages (after 8 seconds)
 unsigned long previousMillis = 0;
 int count = 0;
 
+TinyGPSPlus gps; // gps object
+SoftwareSerial ss(RxPin, TxPin); // comm btn arduino and gps
 WiFiClient wifiClient; // creates a wifi client
 MqttClient mqttClient(wifiClient); // connects the wifi client to the mqtt client
 
 void setup() {
   Serial.begin(9600);
+  ss.begin(GPSBaud);
+  
   while (!Serial) {
     ; // wait for USB port to be connected
   }
@@ -43,6 +57,32 @@ void setup() {
 }
 
 void loop() {
+  // getting gps data
+  DynamicJsonDocument doc(1024); // arduino json
+  String locationData; // location data in JSON format
+  
+  while (ss.available() > 0) {
+    if (gps.encode(ss.read())) {
+      // handle the gps data
+      if (gps.date.isValid()) {
+        doc["date"] = gps.date.day() + '/' + gps.date.month() + '/' + gps.date.year();
+      }
+      if (gps.time.isValid()) {
+        doc["time"] = gps.time.hour() + ':' + gps.time.minute() + ':' + gps.time.second();
+      }
+      if (gps.location.isValid()) {
+        doc["coords"][0] = gps.location.lat();
+        doc["coords"][1] = gps.location.lng(); 
+      }
+      serializeJson(doc, locationData); // prints the JSON object to a string
+    }
+
+    if (millis() > 5000 && gps.charsProcessed() < 10) {
+      Serial.println("No GPS detected: check wiring.");
+      while(true);
+    }
+  }
+  
   mqttClient.poll(); // polling sends keep alive in order to avoid being disconnected to the broker
   unsigned long currentMillis = millis();
 
@@ -50,10 +90,16 @@ void loop() {
     previousMillis = currentMillis;
     Serial.print("Sending location data to topic: ");
     Serial.println(topic);
-    Serial.println();
+    Serial.println(locationData); // print the gps location data
     mqttClient.beginMessage(topic);
-    mqttClient.print("Hello world");
+    mqttClient.print(locationData); // send the gps location data
     mqttClient.endMessage();
     Serial.println();
   }
 }
+
+// References
+// https://docs.arduino.cc/tutorials/uno-wifi-rev2/uno-wifi-r2-mqtt-device-to-device
+// https://create.arduino.cc/projecthub/ruchir1674/how-to-interface-gps-module-neo-6m-with-arduino-8f90ad
+// https://github.com/mikalhart/TinyGPSPlus/blob/master/examples/DeviceExample/DeviceExample.ino
+// https://arduinojson.org/?utm_source=meta&utm_medium=library.properties
