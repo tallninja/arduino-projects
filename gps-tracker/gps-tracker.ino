2ad42007817ab92e453430eac6fd31926f25a354
Ernest Wambua <ernestwambua2@gmail.com>
2022-07-15 07:46:31 +0300

    Changed the wifi library

--- gps-tracker/gps-tracker.ino
+++ gps-tracker/gps-tracker.ino
@@ -7,62 +7,52 @@
 #include <TinyGPSPlus.h>
 #include <SoftwareSerial.h>
 #include <ArduinoMqttClient.h> // MQTT library
-#include <WiFiNINA.h> // Wi-Fi library
+#include <WiFiEsp.h> // Wi-Fi library
+#include <WiFiEspUdp.h>
+#include <WiFiEspClient.h>
 #include "wifi_secrets.h"
 
-const int RxPin = 0, TxPin = 1;
 const uint32_t GPSBaud = 4800;
 char *wifi_ssid = WIFI_SSID;
 char *wifi_password = WIFI_PASSWORD;
+int wifi_status = WL_IDLE_STATUS;
 const char *mqtt_broker = "test.mosquitto.org"; // mqtt broker
 int port = 1883; // tcp port
 const char *topic = "gps_location";
-const int interval =  8000; // interval for sending messages (after 8 seconds)
+const int interval =  8000; // interval for sending megps_ssages (after 8 seconds)
 unsigned long previousMillis = 0;
 int count = 0;
 
 TinyGPSPlus gps; // gps object
-SoftwareSerial ss(RxPin, TxPin); // comm btn arduino and gps
-WiFiClient wifiClient; // creates a wifi client
+SoftwareSerial gps_ss(0, 1); // RX, TX
+SoftwareSerial wifi_ss(2, 3); // RX, TX
+WiFiEspClient wifiClient; // creates a wifi client
 MqttClient mqttClient(wifiClient); // connects the wifi client to the mqtt client
 
 void setup() {
   Serial.begin(9600);
-  ss.begin(GPSBaud);
-  
-  while (!Serial) {
-    ; // wait for USB port to be connected
-  }
-
-  // attempt to connect to the wifi network
-  Serial.print("Connecting to WiFi network: ");
-  Serial.println(wifi_ssid);
-  while (WiFi.begin(wifi_ssid, wifi_password) != WL_CONNECTED) {
-    Serial.print(".");
-    delay(3000);
-  }
-  Serial.println("Connected to WiFi network...");
-  Serial.println();
-
-  // attempt to connect to mqtt broker
-  Serial.print("Connecting to MQTT broker: ");
-  Serial.println(mqtt_broker);
-  if (!mqttClient.connect(mqtt_broker, port)) {
-    Serial.print("Error connecting to MQTT broker: ");
-    Serial.println(mqttClient.connectError());
-    while (1);
-  }
-  Serial.println("Connected to MQTT broker...");
-  Serial.println();
+  gps_ss.begin(GPSBaud);
+  while (!Serial); // wait for USB port connection
+  initWifi(); // connect to WiFi network
+  connectMqtt(); // connect to mqtt broker
 }
 
 void loop() {
   // getting gps data
   DynamicJsonDocument doc(1024); // arduino json
   String locationData; // location data in JSON format
+  wifi_status = WiFi.status();
+  // attempt to reconnect to wifi if we loose connection
+  if (wifi_status != WL_CONNECTED) {
+    connectWifi();
+  }
+  // attempt to reconnect to mqtt broker if we loose connection
+  if (!mqttClient.connected()) {
+    connectMqtt();
+  }
   
-  while (ss.available() > 0) {
-    if (gps.encode(ss.read())) {
+  while (gps_ss.available() > 0) {
+    if (gps.encode(gps_ss.read())) {
       // handle the gps data
       if (gps.date.isValid()) {
         doc["date"] = gps.date.day() + '/' + gps.date.month() + '/' + gps.date.year();
@@ -98,8 +88,48 @@ void loop() {
   }
 }
 
+// WiFi connection
+void initWifi() {
+  wifi_ss.begin(9600); // initialize serial for ESP WiFi module
+  WiFi.init(&wifi_ss); // initialize ESP module
+  // check if WiFi module is connected
+  wifi_status = WiFi.status();
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("Wifi shiled not present");
+    while (true); // don't continue with execution
+  }
+  connectWifi();
+}
+
+void connectWifi() {
+  // attempt to connect to the wifi network
+  Serial.print("Connecting to WiFi network: ");
+  Serial.println(wifi_ssid);
+  while (wifi_status != WL_CONNECTED) {
+    Serial.print(".");
+    wifi_status = WiFi.begin(wifi_ssid, wifi_password);
+    delay(500); // wait for 500 millis to attempt to reconnect to WiFi network
+  }
+  Serial.println("Connected to WiFi network...");
+  Serial.println();
+}
+
+// attempt to connect to mqtt broker
+void connectMqtt() {
+  Serial.print("Connecting to MQTT broker: ");
+  Serial.println(mqtt_broker);
+  if (!mqttClient.connect(mqtt_broker, port)) {
+    Serial.print("Error connecting to MQTT broker: ");
+    Serial.println(mqttClient.connectError());
+    while (1);
+  }
+  Serial.println("Connected to MQTT broker...");
+  Serial.println();
+}
+
 // References
 // https://docs.arduino.cc/tutorials/uno-wifi-rev2/uno-wifi-r2-mqtt-device-to-device
 // https://create.arduino.cc/projecthub/ruchir1674/how-to-interface-gps-module-neo-6m-with-arduino-8f90ad
 // https://github.com/mikalhart/TinyGPSPlus/blob/master/examples/DeviceExample/DeviceExample.ino
 // https://arduinojson.org/?utm_source=meta&utm_medium=library.properties
+// https://thingsboard.io/docs/samples/arduino/temperature/
